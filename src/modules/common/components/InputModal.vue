35c44a8640430c83d37c3a989163452d11923d91
gerodrig <garc@outlook.com>
2024-05-28 14:43:30 -0600

    first commit

---
+++ src/modules/common/components/InputModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <dialog class="modal" :open="open">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p v-if="subTitle" class="py-4">{{ subTitle }}</p>
+
+      <div class="flex flex-col modal-action">
+        <form method="dialog" @submit.prevent="submitValue">
+          <input
+            ref="inputRef"
+            type="text"
+            :placeholder="placeholder ?? 'Ingrese un valor'"
+            class="flex-1 w-full input input-bordered input-primary"
+            v-model="inputValue"
+          />
+          <!-- if there is a button in form, it will close the modal -->
+          <div class="flex justify-end mt-5">
+            <button @click="$emit('close')" class="mr-4 btn">Close</button>
+            <button type="submit" class="btn btn-primary">Aceptar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </dialog>
+
+  <div
+    v-if="open"
+    class="fixed top-0 left-0 z-10 w-screen h-screen bg-black modal-backdrop opacity-40"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+type Props = {
+  open: boolean;
+  title: string;
+  subTitle?: string;
+  placeholder?: string;
+};
+
+defineProps<Props>();
+
+const emits = defineEmits<{
+  close: [void];
+  value: [text: string];
+}>();
+
+const inputValue = ref('');
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const submitValue = () => {
+  if (!inputValue.value) {
+    inputRef.value?.focus();
+    return;
+  }
+
+  emits('value', inputValue.value);
+  emits('close');
+};
+</script>
---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col">
+    <!-- //Top Menu -->
+    <top-menu />
+
+    <main class="flex flex-row">
+      <!-- Side Menu -->
+      <side-menu />
+
+      <!-- Router View -->
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '@/modules/projects/components/SideMenu.vue';
+</script>
